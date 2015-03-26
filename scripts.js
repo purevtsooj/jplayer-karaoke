2f4ff3edf1ef5ce98db25cbe8b060f9b84d430b2
purevtsooj <purevtsooj@gmail.com>
2015-03-26 17:18:54 +0800

    fixed 405 error

--- scripts.js
+++ scripts.js
@@ -13,33 +13,33 @@
                 });
                 $this.append($('<div id="karaoke_lyrics" data-index="-1"><span class="word">jPlayer, HTML, CSS3 Karaoke</span></div>'));
 
-                $.post( $this.jPlayer("option", "lyrics"), {}, function(data){
-                    window.karaoke_stimes = [];
-                    window.karaoke_etimes = [];
-                    window.karaoke_current_index = -1;
-                    window.karaoke = $(data);
+                var data = $("#lyrics").html();
+                window.karaoke_stimes = [];
+                window.karaoke_etimes = [];
+                window.karaoke_current_index = -1;
+                window.karaoke = $(data);
 
-                    $(data).find('karaoke > add').each(function(index){
-                        var st = $(this).attr('startTime');
-                        var et = $(this).attr('endTime');
+                $(data).find('karaoke > add').each(function(index){
+                    var st = $(this).attr('startTime');
+                    var et = $(this).attr('endTime');
 
-                        var sts = st.split(':');
-                        st = parseFloat(sts[0], 10)*60*60 + parseFloat(sts[1], 10)*60 + parseFloat(sts[2]);
-                        $(this).attr('startTime' , st);
-                        window.karaoke_stimes.push(st);
+                    var sts = st.split(':');
+                    st = parseFloat(sts[0], 10)*60*60 + parseFloat(sts[1], 10)*60 + parseFloat(sts[2]);
+                    $(this).attr('startTime' , st);
+                    window.karaoke_stimes.push(st);
 
-                        var ets = et.split(':');
-                        et = parseFloat(ets[0], 10)*60*60 + parseFloat(ets[1], 10)*60 + parseFloat(ets[2]);
-                        $(this).attr('endTime' , et);
-                        window.karaoke_etimes.push(et);
-                    });
-
-                    $this.jPlayer("play");
-
-                    setTimeout(function(){
-                        $('#karaoke_lyrics').html($(data).find('details singer').text() + ' - ' + $(data).find('details name').text());
-                    }, 3000);
+                    var ets = et.split(':');
+                    et = parseFloat(ets[0], 10)*60*60 + parseFloat(ets[1], 10)*60 + parseFloat(ets[2]);
+                    $(this).attr('endTime' , et);
+                    window.karaoke_etimes.push(et);
                 });
+
+                $this.jPlayer("play");
+
+                setTimeout(function(){
+                    $('#karaoke_lyrics').html($(data).find('details singer').text() + ' - ' + $(data).find('details name').text());
+                }, 3000);
+
             },
             lyrics: "media/gatsuurhan.xml",
             swfPath: "jplayer/",
